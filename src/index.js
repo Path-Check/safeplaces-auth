1faa4275e92ecf97628fba4d9e99410a6522dbd9
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-06-30 17:36:36 -0400

    Add examples to README and change exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const strategies = require('./strategies');
+const handlers = require('./handlers');
 const Enforcer = require('./enforcer');
 const Issuer = require('./issuer');
+const JWKSClient = require('./jwksclient');
 
 module.exports = {
   Enforcer,
   Issuer,
+  JWKSClient,
   strategies,
+  handlers,
 };
